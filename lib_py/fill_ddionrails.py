b0d2d4cf5e84f262ca83ee19f96867d9b9a86fec
afuetterer <fuetterh@posteo.de>
2019-08-19 15:14:02 +0200

    Housekeeping
    - remove bibtex.bib
    - copy files from metadata to ddionrails
    - add "topic" column to concepts.csv
    - rename "logical_datasets.csv" to "datasets.csv"
    - rename column "dataset" to "name" in "datasets.csv"
    - rename "logical_variables.csv" to "variables.csv"
    - rename column "variable" to "name" in "variables.csv"

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -1,12 +1,40 @@
+import pathlib
+import shutil
+
 from ddi.onrails.repos import convert_r2ddi, copy, dor1
+
+STUDY = "soep-base"
+VERSION = "v1"
+
+INPUT_DIRECTORY = pathlib.Path("metadata")
+OUTPUT_DIRECTORY = pathlib.Path("ddionrails")
 
 
 def main():
     copy.study()
     dor1.datasets()
     dor1.variables()
-    convert_r2ddi.Parser("soep-base", version="v1").write_json()
-    copy.bibtex()
+    convert_r2ddi.Parser(STUDY, version=VERSION).write_json()
+    shutil.copy(
+        INPUT_DIRECTORY.joinpath("analysis_units.csv"),
+        OUTPUT_DIRECTORY.joinpath("analysis_units.csv"),
+    )
+    shutil.copy(
+        INPUT_DIRECTORY.joinpath("concepts.csv"),
+        OUTPUT_DIRECTORY.joinpath("concepts.csv"),
+    )
+    shutil.copy(
+        INPUT_DIRECTORY.joinpath("conceptual_datasets.csv"),
+        OUTPUT_DIRECTORY.joinpath("conceptual_datasets.csv"),
+    )
+    shutil.copy(
+        INPUT_DIRECTORY.joinpath("periods.csv"),
+        OUTPUT_DIRECTORY.joinpath("periods.csv"),
+    )
+    shutil.copy(
+        INPUT_DIRECTORY.joinpath("publications.csv"),
+        OUTPUT_DIRECTORY.joinpath("publications.csv"),
+    )
 
 
 if __name__ == "__main__":
